57604cbdeddc8000b23076de222f534f968d591a
Aysegul <aysglbabacan@gmail.com>
2023-01-28 16:17:14 +0300

    ödev

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,72 @@
+const userModel = require("../users/users-model");
+
 function logger(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  const method = req.method; //Get Post Put ...
+  const url = req.originalUrl; // /api/users , /api/user/7 vb
+  const timeStamp = new Date().toLocaleString();
+  console.log(`[${timeStamp}] , ${method} -- ${url}`);
+  //cevap yollaması için next dedik
+  next();
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  try{
+    const user = await userModel.getById(req.params.id);
+    if(!user){
+      res.status(404).json({
+        message :"not found"
+      })
+      //cevap yollarsak next yazma
+    }
+    else{
+      req.user = user;
+      //get/:id de sadece req.user yaparak cevap yollarız
+    }
+    next();
+    //cevap için next (response olduğunda next yazmak zorunda değiliz.)
+  }
+  catch(err){
+    res.status(500).json({
+      message: "İslem yapılamadı"
+    })
+  }
+
 }
 
 function validateUser(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  // gelen body kontrolunu burada yaptık
+  const { name } = req.body;
+  if(!name){
+    res.status(400).json({
+      message:"gerekli name alanı eksik"
+    })
+  }
+  else{
+    req.name = name;  //önce body içinden name i aldık sonra bunu kullandık. req.name i bizim bodyden aldığımız name e atadık.
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  const {text} = req.body;
+  if(!text){
+    res.status(400).json({
+      message:"text eksik"
+    })
+  }
+  else{
+    req.text=text;
+    next();
+  }
 }
 
 // bu işlevleri diğer modüllere değdirmeyi unutmayın
+
+module.exports = {logger, 
+  validateUserId, 
+  validateUser, 
+  validatePost}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,117 @@
 const express = require('express');
-
+const {logger,validatePost,validateUserId,validateUser} = require("../middleware/middleware");
 // `users-model.js` ve `posts-model.js` sayfalarına ihtiyacınız var
 // ara yazılım fonksiyonları da gereklidir
 
 const router = express.Router();
+const userModel = require("./users-model");
+const postModel = require("../posts/posts-model");
 
-router.get('/', (req, res) => {
+router.get('/',(req, res, next) => {
   // TÜM KULLANICILARI İÇEREN DİZİYİ DÖNDÜRÜN
+  userModel.get()
+  .then( users => {
+    res.json(users)
+  })
+  .catch(next)
+  //burada next dedik, sayfanın sonundaki error middleware ine gitsin diye
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId,(req, res, next) => {
+  //validateUserId ilk çalıscak
   // USER NESNESİNİ DÖNDÜRÜN
   // user id yi getirmek için bir ara yazılım gereklidir
+  res.json(req.user);
+  //req.user değeri validateUserId den geliyor
+
 });
 
-router.post('/', (req, res) => {
+router.post('/',validateUser, (req, res, next) => {
   // YENİ OLUŞTURULAN USER NESNESİNİ DÖNDÜRÜN
   // istek gövdesini doğrulamak için ara yazılım gereklidir.
+  userModel.insert({name: req.name})
+  .then( newUser =>{
+    res.status(200).json(newUser);
+  })
+  .catch(next)
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',validateUserId, validateUser, async (req, res, next) => {
+  //validateUserId yaptıktan sonra next ile validatePost kısmına geçicek
+
   // YENİ GÜNCELLENEN USER NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan ara yazılım gereklidir
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
+
+  try{
+    //2 kontrolde yapıldı
+    await userModel.update(req.params.id, {name: req.name});
+    const updatedUser = await userModel.getById(req.params.id);
+    res.status(201).json(updatedUser);
+  }
+  catch(err){
+    next(err);//try-catch yapısı olduğu için böyle yazdık.
+  }
+
+  
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',validateUserId, async(req, res, next) => {
   // SON SİLİNEN USER NESNESİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
+  try{
+   await userModel.remove(req.params.id);
+  res.json(req.user); 
+    // const deletedUser = await userModel.getById(req.params.id);
+    // await userModel.remove(req.params.id);
+    // res.json(deletedUser);
+  }
+  catch(err){
+    next(err)
+  }
+
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts',validateUserId, async(req, res,next) => {
   // USER POSTLARINI İÇEREN BİR DİZİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
+  try{
+    const result = await userModel.getUserPosts(req.params.id);
+    res.json(result);
+  }
+  catch(err){
+    next(err);
+  }
+
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts',validateUserId,validatePost, async(req, res, next) => {
   // YENİ OLUŞTURULAN KULLANICI NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
+
+  try{
+    const result = await postModel.insert({
+      user_id:req.params.id, //req.user demedik bütün kullanıcıyı alır diye
+      text:req.text
+    });
+    res.json(result);
+
+  }
+  catch(err){
+    next(err);
+  }
+
 });
 
+
+
+router.use((err,req,res,next) => {
+  res.status(err.status || 500).json({
+    customMessage: "Birseyler yanlıs gitti",
+    message: err.message
+  });
+});
 // routerı dışa aktarmayı unutmayın
+
+module.exports = router;
